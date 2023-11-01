d20d3aeb7155909911abb103075391d00005be53
M1martina <atanasovskamartina954@gmail.com>
2023-11-01 09:53:24 -0400

    Had to delete pretty report because couldn't run cukes Made changes in Step Definitonts Created Deck Module page

--- src/test/java/com/tryCloud/step_definitions/DeckModule_Step_Definitions.java
+++ src/test/java/com/tryCloud/step_definitions/DeckModule_Step_Definitions.java
@@ -1,4 +1,82 @@
 package com.tryCloud.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class DeckModule_Step_Definitions {
+
+    @Given("user enters login and password")
+    public void user_enters_login_and_password() {
+
+    }
+    @When("user clicks on Deck Module")
+    public void user_clicks_on_deck_module() {
+
+    }
+    @When("then clicks on {string}")
+    public void then_clicks_on(String string) {
+
+    }
+    @When("writes a new name")
+    public void writes_a_new_name() {
+
+    }
+    @Then("newly created board should be displayed under {string}")
+    public void newly_created_board_should_be_displayed_under(String string){
+
+    }
+
+
+    \
+    @When("user chooses a board name")
+    public void user_chooses_a_board_name() {
+
+    }
+    @When("clicks on Add List Button")
+    public void clicks_on_add_list_button() {
+
+    }
+    @When("writes a new list name")
+    public void writes_a_new_list_name() {
+
+    }
+    @When("hits enter on the keyboard")
+    public void hits_enter_on_the_keyboard() {
+
+    }
+    @Then("new list should be displayed")
+    public void new_list_should_be_displayed() {
+
+    }
+
+
+
+    @When("clicks Add card button")
+    public void clicks_add_card_button() {
+
+    }
+    @When("enters a new Card name")
+    public void enters_a_new_card_name() {
+
+    }
+    @Then("new card should be displayed")
+    public void new_card_should_be_displayed() {
+
+    }
+
+
+    @When("clicks on the three dots on the {string}")
+    public void clicks_on_the_three_dots_on_the(String string) {
+
+    }
+    @When("clicks on Assign to me button")
+    public void clicks_on_assign_to_me_button() {
+
+    }
+    @Then("user profile icon should be displayed near the three dots icon")
+    public void user_profile_icon_should_be_displayed_near_the_three_dots_icon() {
+
+    }
+
 }
