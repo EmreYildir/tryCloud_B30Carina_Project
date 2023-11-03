781dc338f4c7b54f13b65eeae981086e44fb47c4
EmreYildir <emyildir@gmail.com>
2023-11-03 15:54:09 +0300

    Test Scenarios and implementation update 4

--- src/test/java/com/tryCloud/pages/DashboardPage.java
+++ src/test/java/com/tryCloud/pages/DashboardPage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import javax.xml.xpath.XPath;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DashboardPage {
 
@@ -16,6 +18,12 @@ public class DashboardPage {
     @FindBy(xpath = "(//li/a[@aria-label='Files'])[1]")
     public WebElement filesButton;
 
+    @FindBy(xpath = "//ul[@id='appmenu']/li//a")
+    public List<WebElement> dashBarModulesList = new ArrayList<>();
+
+    @FindBy(xpath = "//div[@class='avatardiv avatardiv-shown']")
+    public WebElement avatar;
+
     @FindBy(xpath = "//span[@class='user-status-menu-item__header']")
     public WebElement usernameHeader;
 
@@ -23,7 +31,22 @@ public class DashboardPage {
     public WebElement customizeButton;
 
     @FindBy(xpath = "//li[@data-v-d8d86ca2]")
-    public WebElement widgetBoxes;
+    public List<WebElement> widgetBoxes;
+
+    @FindBy(xpath = "//ul[@class='statuses']//div[starts-with(@id, 'status-')]")
+    public List<WebElement> getWidgetHeaderList1 = new ArrayList<>();
+
+    @FindBy(xpath = "//div[@class='panel--header']")
+    public List<WebElement> widgetHeaderList2 = new ArrayList<>();
+
+    @FindBy(xpath = "(//button)[1]")
+    public WebElement setStatusButton;
+
+    @FindBy(xpath = "//div[@class='user-status-online-select']")
+    public List<WebElement> statusList = new ArrayList<>();
+
+    @FindBy(xpath = "//button[@class='status-buttons__primary primary']")
+    public WebElement setStatusMessageButton;
 
 
 
